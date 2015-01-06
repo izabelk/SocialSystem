fce3d2078aa12288b2dceecff1a17a5146809442
izabelk <izi_girl@abv.bg>
2015-01-06 12:32:43 +0200

    Added some files for client side login

---
+++ SocialSystem/SocialSystem/public/app/account/identity.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+app.factory('identity', function ($window, UsersResource) {
+    var user;
+    var token;
+ 
+    if ($window.bootstrappedUserObject) {
+        user = new UsersResource();
+        angular.extend(user, $window.bootstrappedUserObject);
+    }
+    return {
+        currentUser: user,
+        token: token,
+        isAuthenticated: function () {
+            return !!this.currentUser;
+        }
+    };
+});
---
+++ SocialSystem/SocialSystem/public/app/app.js
@@ -0,0 +1,52 @@
+﻿'use strict';
+
+var app = angular.module('app', ['ngResource', 'ngRoute'])
+                 .value('toastr', toastr);
+
+app.config(function ($routeProvider) {
+    
+    //var routeUserChecks = {
+    //    authenticated: {
+    //        authenticate: function (auth) {
+    //            return auth.isAuthenticated();
+    //        }
+    //    }
+    //};
+    
+    //$routeProvider
+    //    .when('/', {
+    //    templateUrl: '/partials/main/home',
+    //    controller: 'MainCtrl'
+    //})
+    //    .when('/login', {
+    //    templateUrl: '/partials/account/login',
+    //    controller: 'LoginCtrl'
+    //})
+    //    .when('/users/:id', {
+    //    templateUrl: '/partials/users/user-details',
+    //    controller: 'UserDetailsCtrl'
+    //})
+    //    .when('/inbox', {
+    //    templateUrl: '/partials/messages/inbox',
+    //    controller: 'InboxCtrl',
+    //    resolve: routeUserChecks.authenticated
+    //})
+    //    .when('/send-message/:username', {
+    //    templateUrl: '/partials/messages/send-message-form',
+    //    controller: 'SendMessageCtrl',
+    //    resolve: routeUserChecks.authenticated
+    //})
+    //    .when('/signup', {
+    //    templateUrl: '/partials/account/signup',
+    //    controller: 'SignUpCtrl'
+    //})
+});
+
+//app.run(function ($rootScope, $window, notifier) {
+//    $rootScope.$on('$routeChangeError', function (ev, current, previous, rejection) {
+//        if (rejection === 'not authorized') {
+//            notifier.error('You are not authorized!');
+//            $window.history.back();
+//        }
+//    });
+//});
